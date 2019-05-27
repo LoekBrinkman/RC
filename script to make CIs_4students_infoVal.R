6333858b880326e173cc9e9cc6de293e29f0d100
LoekBrinkman <l.brinkman@uu.nl>
2019-05-27 13:50:27 +0200

    adjusted for data of A&D

--- script to make CIs_4students_infoVal.R
+++ script to make CIs_4students_infoVal.R
@@ -15,29 +15,29 @@ rm(list=ls())
 setwd("~/ci script")
 
 #load data
-dat <- read_csv2('data_exp_7614-v16_task-zaz4.csv')
+dat <- read_csv('data_exp_8140-v4_task-q3sn.csv')
 
 dat <- dat %>% select(`Participant Public ID`, Response, display, `Trial Number`) %>% #select the Response column
   filter(!is.na(Response) & Response != "null") %>% #remove rows that don't code for stimuli 
   filter(display == "Choose") %>% 
-  mutate(Response_binary = ifelse(Response == 'Foto 1', 1, -1)) #recode responses: ori = 1, inv = -1
+  mutate(Response_binary = ifelse(Response == 'Photo 1', 1, -1)) #recode responses: ori = 1, inv = -1
 
-#loop over participants & compute CIs
-for (i in unique(dat$`Participant Public ID`)){ #loop over participants
-  filename <- i 
-  if (!file.exists(str_c(getwd(), '/cis/ci_', filename, '.png'))) { #check whether the CI already exists for this participant, if so, skip this participant.
-    print(str_c('Computing CI for ppn ', filename))
-    response_tmp <- dat %>% filter(`Participant Public ID` == i) #select the trials for a single participant
-    
-    generateCI(stimuli = 1:500, #this is where the magic happens!
-               responses = response_tmp$Response_binary, 
-               baseimage = 'base_face_FMNES', 
-               rdata = 'rcic_seed_1_time_Feb_20_2019_13_42.RData',
-               filename = filename)
-  } else {
-    print(str_c('CI for ppn ', filename, ' already exists'))
-  }  
-}
+# #loop over participants & compute CIs
+# for (i in unique(dat$`Participant Public ID`)){ #loop over participants
+#   filename <- i 
+#   if (!file.exists(str_c(getwd(), '/cis/ci_', filename, '.png'))) { #check whether the CI already exists for this participant, if so, skip this participant.
+#     print(str_c('Computing CI for ppn ', filename))
+#     response_tmp <- dat %>% filter(`Participant Public ID` == i) #select the trials for a single participant
+#     
+#     generateCI(stimuli = 1:500, #this is where the magic happens!
+#                responses = response_tmp$Response_binary, 
+#                baseimage = 'base_face_FMNES', 
+#                rdata = 'rcic_seed_1_time_Feb_20_2019_13_42.RData',
+#                filename = filename)
+#   } else {
+#     print(str_c('CI for ppn ', filename, ' already exists'))
+#   }  
+# }
 
 
 ## compute infoVal score per participant
